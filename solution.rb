8150bd5ece5f0cc18a66c524f1ad9545b1c6c60b
Juan Pablo <dupjpr@gmail.com>
2020-05-06 17:27:21 -0500

    2 update

--- solution.rb
+++ solution.rb
@@ -2,11 +2,9 @@ require "sinatra"
 
 class Check
 
-
-
   def each
     
-    (1..50).map{|i| i.even? ? "<h1>#{i} Soy par!</h1>" : "<h1>#{i} Soy impar!</h1>" }
+    (1..50).map{|i| i.even? ? "#{i} Soy par!" : "#{i} Soy impar!" }
 
   end
 
@@ -15,5 +13,32 @@ end
 num = Check.new
   
 get "/" do
-  num.each
+  @num = num.each
+  erb :index
 end
+
+
+
+
+
+
+
+# require "sinatra"
+
+# class Check
+
+
+
+#   def each
+    
+#     (1..50).map{|i| i.even? ? "<h1>#{i} Soy par!</h1>" : "<h1>#{i} Soy impar!</h1>" }
+
+#   end
+
+# end
+
+# num = Check.new
+  
+# get "/" do
+#   num.each
+# end
